8211fd1b4e75ab6cdc0c733a00bd177e2856db62
Horst Hettrich <horst_hettrich@gmx.de>
2017-08-23 17:07:33 +0200

    create pip package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='PyVerilog',
+      version='0.1',
+      description='Verilog Parser for Python',
+      url='https://github.com/yellekelyk/PyVerilog',
+      author='yellekelyk',
+      packages=['PyVerilog'],
+      zip_safe=False)
